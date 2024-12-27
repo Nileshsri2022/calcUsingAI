0c473f38437b90e8840f046ee554344417a6e798
Nileshsri2022 <nilesh.srivastava.51273@gmail.com>
2024-12-27 21:24:49 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import '@mantine/core/styles.css';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import {MantineProvider} from '@mantine/core';
+import Home from './screens/home';
+const paths = [
+  {
+    path: '/',
+    element:(
+      <Home/>
+    ),
+  },
+];
+const BrowserRouter = createBrowserRouter(paths);
+
+const App = () => {
+  return (
+    <MantineProvider>
+      <RouterProvider router={BrowserRouter}/>
+    </MantineProvider>
+  );
+}
+export default App;
